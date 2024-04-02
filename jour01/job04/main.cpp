53246c7e5e8db2328d0f7dc7969b75e9a2ba0037
thibault-kine <91733294+thibault-kine@users.noreply.github.com>
2024-04-02 13:02:10 +0200

    job04 done

---
+++ jour01/job04/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Entrez un nombre : ";
+    int num;
+    std::cin >> num;
+
+    num % 2 == 0 ?
+        std::cout << num << " est pair" << std::endl
+    :   std::cout << num << " est impair" << std::endl;
+}
